4b87f0ba89af238acbadc58ecae80a506f55ab73
Ronnie Dalsgaard <ronnie.dalsgaard@gmail.com>
2016-05-29 14:18:56 +0200

    Add methods to roomservice

--- src/age/client/IRoomService.java
+++ src/age/client/IRoomService.java
@@ -1,11 +1,18 @@
 package age.client;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+import age.shared.model.Area;
+import age.shared.model.Building;
 import age.shared.model.Room;
 
 @RemoteServiceRelativePath("room")
 public interface IRoomService extends RemoteService {
 	public Room getRoom(int id);
+	public List<Area> getAllAreas();
+	public List<Building> getAllBuildings();
+	public List<Room> getAllRooms();
 }
--- src/age/client/IRoomServiceAsync.java
+++ src/age/client/IRoomServiceAsync.java
@@ -1,10 +1,17 @@
 package age.client;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+import age.shared.model.Area;
+import age.shared.model.Building;
 import age.shared.model.Room;
 
 public interface IRoomServiceAsync {
 	public void getRoom(int id, AsyncCallback<Room> callback);
+	public void getAllAreas(AsyncCallback<List<Area>> callback);
+	public void getAllBuildings(AsyncCallback<List<Building>> callback);
+	public void getAllRooms(AsyncCallback<List<Room>> callback);
 
 }
