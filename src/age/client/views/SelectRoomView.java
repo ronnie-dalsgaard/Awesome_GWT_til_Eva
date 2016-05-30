cdaa7f95f16c01e5141278234546b71350f24e94
Ronnie Dalsgaard <ronnie.dalsgaard@gmail.com>
2016-05-30 14:48:39 +0200

    Add UI bug fixes

--- src/age/client/views/SelectRoomView.java
+++ src/age/client/views/SelectRoomView.java
@@ -5,6 +5,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -23,20 +24,16 @@ import age.shared.model.Building;
 import age.shared.model.Room;
 
 public class SelectRoomView extends Composite {
+	interface SelectRoomViewUiBinder extends UiBinder<Widget, SelectRoomView> {	}
+	
+	private final String LOADING = "Loading...";
+	private final String SELECT = "Select";
+	private static SelectRoomViewUiBinder uiBinder = GWT.create(SelectRoomViewUiBinder.class);
 	
 	public interface ISelectRoomViewCallback {
 		public void OnRoomSelected(Room room);
 	}
 	private ISelectRoomViewCallback callback;
-
-	public void setCallback(ISelectRoomViewCallback callback) {
-		this.callback = callback;
-	}
-
-	private final String LoadString = "Loading...";
-	private static SelectRoomViewUiBinder uiBinder = GWT.create(SelectRoomViewUiBinder.class);
-
-	interface SelectRoomViewUiBinder extends UiBinder<Widget, SelectRoomView> {	}
 
 	@UiField Button button;
 	@UiField Label areaLbl;
@@ -49,16 +46,6 @@ public class SelectRoomView extends Composite {
 	private final IRoomServiceAsync roomService = GWT.create(IRoomService.class);
 	
 	public SelectRoomView() {
-		init();
-	}
-
-
-	public SelectRoomView(ISelectRoomViewCallback callback) {
-		init();
-		this.callback = callback;
-	}
-	
-	private void init(){
 		initWidget(uiBinder.createAndBindUi(this));
 		areaDropDown.addChangeHandler(new AreaDropDownHanlder());
 		buildingDropDown.addChangeHandler(new BuildingDropDownHandler());
@@ -70,16 +57,35 @@ public class SelectRoomView extends Composite {
 		buildingLbl.setText("Vælg bygning");
 		roomLbl.setText("Vælg Værelse");		
 	}
-
-	@UiHandler("button")
+	public SelectRoomView(ISelectRoomViewCallback callback) {
+		this();
+		this.callback = callback;
+	}
+	
+	@UiHandler("button") 
 	void onClick(ClickEvent e) {
-		Window.alert("Hello!");
+		int roomId = Integer.parseInt(roomDropDown.getSelectedValue());
+		roomService.getRoom(roomId, new AsyncCallback<Room>() {
+			
+			@Override
+			public void onSuccess(Room result) {
+				callback.OnRoomSelected(result);
+			}
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert(caught.getMessage());
+			}
+		});
 	}
 
-	
+	public void setCallback(ISelectRoomViewCallback callback) {
+		this.callback = callback;
+	}
 	
 	private void populateAreaDropdown() {
-
+		areaDropDown.clear();
+		areaDropDown.addItem(LOADING);
 		roomService.getAllAreas(new AsyncCallback<List<Area>>() {
 			
 			@Override
@@ -88,8 +94,8 @@ public class SelectRoomView extends Composite {
 				buildingDropDown.clear();
 				areaDropDown.clear();
 				button.setEnabled(false);
-				for(Area a: result)
-				{
+				areaDropDown.addItem(SELECT, "-1");
+				for(Area a: result) {
 					areaDropDown.addItem(a.getName(), Integer.toString(a.getId()));
 				}
 				
@@ -101,9 +107,9 @@ public class SelectRoomView extends Composite {
 			}
 		});
 	}
-	public void populateBuildingDropDown() {
-
-		int areaId = Integer.parseInt(areaDropDown.getSelectedValue());
+	private void populateBuildingDropDown(int areaId) {
+		buildingDropDown.clear();
+		buildingDropDown.addItem(LOADING);
 		roomService.getBuildingsForArea(areaId, new AsyncCallback<List<Building>>() {
 			
 			@Override
@@ -111,6 +117,7 @@ public class SelectRoomView extends Composite {
 				buildingDropDown.clear();
 				roomDropDown.clear();
 				button.setEnabled(false);
+				buildingDropDown.addItem(SELECT, "-1");
 				for(Building b : result){
 					buildingDropDown.addItem(b.getName(), Integer.toString(b.getId()));
 				}
@@ -122,15 +129,17 @@ public class SelectRoomView extends Composite {
 			}
 		});
 	}
-	
-	public void populateRoomDropDown() {
+	private void populateRoomDropDown() {
+		roomDropDown.clear();
+		roomDropDown.addItem(LOADING);
 		int buildingId = Integer.parseInt(buildingDropDown.getSelectedValue());
 		roomService.getRoomsForBuildning(buildingId, new AsyncCallback<List<Room>>() {
 			
 			@Override
 			public void onSuccess(List<Room> result) {
 				roomDropDown.clear();
-				button.setEnabled(!result.isEmpty());
+				button.setEnabled(false);
+				roomDropDown.addItem(SELECT, "-1");
 				for(Room r : result){
 					roomDropDown.addItem(r.getName(), Integer.toString(r.getId()));
 				}
@@ -144,52 +153,35 @@ public class SelectRoomView extends Composite {
 		
 	}
 	
-	private class AreaDropDownHanlder implements ChangeHandler
-	{
-
+	private class AreaDropDownHanlder implements ChangeHandler {
 		@Override
 		public void onChange(ChangeEvent event) {
+			button.setEnabled(false);
+			int areaId = Integer.parseInt(areaDropDown.getSelectedValue());
+			if(areaId < 0) return;
 			buildingDropDown.clear();
 			roomDropDown.clear();
-			button.setEnabled(false);
+			Window.alert("roomDropDown cleared");
 			
-			buildingDropDown.addItem(LoadString);
-			populateBuildingDropDown();
+			buildingDropDown.addItem(LOADING);
+			populateBuildingDropDown(areaId);
 		}
-		
 	}
-
-	private class BuildingDropDownHandler implements ChangeHandler
-	{
-
+	private class BuildingDropDownHandler implements ChangeHandler {
 		@Override
 		public void onChange(ChangeEvent event) {
 			roomDropDown.clear();
 			button.setEnabled(false);
-			roomDropDown.addItem(LoadString);
+			roomDropDown.addItem(LOADING);
 			populateRoomDropDown();
 		}
 	}
-	
-	private class RoomDropdDownHandler implements ChangeHandler{
-
+	private class RoomDropdDownHandler implements ChangeHandler {
 		@Override
 		public void onChange(ChangeEvent event) {
 			int roomId = Integer.parseInt(roomDropDown.getSelectedValue());
-			roomService.getRoom(roomId, new AsyncCallback<Room>() {
-				
-				@Override
-				public void onSuccess(Room result) {
-					callback.OnRoomSelected(result);
-				}
-				
-				@Override
-				public void onFailure(Throwable caught) {
-					Window.alert(caught.getMessage());
-				}
-			});
+			button.setEnabled(roomId >= 0);
 		}
-		
 	}
 }
 
